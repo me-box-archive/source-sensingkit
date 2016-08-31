4f3a9f25c2f1f3e2193be878d6fbc5d0e476b07d
Yousef Amar <yousefamar@gmail.com>
2016-08-31 15:53:14 +0100

    Implement routing and activate all sensors

--- app/src/main/java/io/amar/databoxsource/MainActivity.java
+++ app/src/main/java/io/amar/databoxsource/MainActivity.java
@@ -9,7 +9,13 @@ import org.sensingkit.sensingkitlib.*;
 import org.sensingkit.sensingkitlib.data.SKLightData;
 import org.sensingkit.sensingkitlib.data.SKSensorData;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -17,6 +23,9 @@ import fi.iki.elonen.NanoHTTPD;
  * Created by amar on 30/08/16.
  */
 public class MainActivity extends Activity {
+
+    SensingKitLibInterface mSensingKitLib;
+    Map<String, SKSensorType> string2sensor = new HashMap<>();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,19 +35,15 @@ public class MainActivity extends Activity {
         log.setMovementMethod(new ScrollingMovementMethod());
 
         try {
-            SensingKitLibInterface mSensingKitLib = SensingKitLib.getSensingKitLib(this);
-            mSensingKitLib.registerSensor(SKSensorType.LIGHT);
-            mSensingKitLib.subscribeSensorDataListener(SKSensorType.LIGHT,
-                    new SKSensorDataListener() {
-                        @Override
-                        public void onDataReceived(final SKSensorType moduleType, final SKSensorData sensorData) {
-                            // We have data!
-                            log(Float.toString(((SKLightData) sensorData).getLight()));
-                        }
-                    });
-            mSensingKitLib.startContinuousSensingWithSensor(SKSensorType.LIGHT);
+            mSensingKitLib = SensingKitLib.getSensingKitLib(this);
+
+            for (SKSensorType sensorType : SKSensorType.values()) {
+                mSensingKitLib.registerSensor(sensorType);
+                mSensingKitLib.startContinuousSensingWithSensor(SKSensorType.LIGHT);
+            }
         } catch (SKException e) {
         }
+
 
         new NanoHTTPD(8080) {
             {
@@ -47,11 +52,42 @@ public class MainActivity extends Activity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                Map<String, Runnable> handlers = new HashMap<>();
+
+                for (SKSensorType sensorType : SKSensorType.values()) {
+                    System.out.println(sensorType.toString().toLowerCase());
+                    handlers.put(sensorType.toString().toLowerCase(), () -> {
+                                try {
+                                    MainActivity.this.mSensingKitLib.subscribeSensorDataListener(sensorType,
+                                            (final SKSensorType moduleType, final SKSensorData sensorData) ->
+                                                    sensorData.getDataInCSV()
+                                    );
+                                } catch (SKException e) {
+                                }
+                            }
+                    );
+                }
+            }
+
+            private String[] parseURI(String uri) {
+                if (uri == null)
+                    return new String[]{};
+                if (uri.startsWith("/"))
+                    uri = uri.substring(1);
+                if (uri.endsWith("/"))
+                    uri = uri.substring(0, uri.length() - 1);
+
+                uri = uri.toLowerCase();
+                return uri.split("/");
             }
 
             @Override
             public Response serve(IHTTPSession session) {
-                return newFixedLengthResponse("<html><body><h1>Hello world!</h1></body></html>\n");
+                String[] path = parseURI(session.getUri());
+                InputStream inputStream = new ByteArrayInputStream("aaaaaaaa".getBytes(Charset.forName("UTF-8")));
+
+                return newChunkedResponse(Response.Status.OK, "text/csv", inputStream);
             }
         };
     }
